bd6da4934cdc1035d415fada09966c2d893d38fa
3172236129 <3172236129@qq.com>
2020-04-26 17:03:34 +0800

    新接口添加

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
-
+var pool = require("../database/connect");
+var dataConnect = require("../database/index")
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/getUser', function(req, res, next) {
+  // res.send('respond with a resource');
+  let name = "luffy"
+  dataConnect(function (conn) {
+      conn.query(`select * from user where name = "${name}"`, function (error, results, fields) {
+        try {
+          res.send(results);
+          conn.release();
+          if (error) throw error;
+        } catch (error) {
+          console.log(error)
+        }
+      })
+  })
+});
+router.get('/getUser1', function(req, res, next) {
+  // res.send('respond with a resource');
+  let name = ["luffy"]
+  dataConnect(function (conn) {
+    conn.query('select * from user where name = ?', name, function (error, results, fields) {
+      res.send(results);
+      conn.release();
+      if (error) throw error;
+    })
+  })
 });
 
 module.exports = router;
