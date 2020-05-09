4a272f1d002da1608ee391b37f1ed77b3ad0ab52
3172236129 <3172236129@qq.com>
2020-05-09 14:51:36 +0800

    nodemon热启动安装

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,13 @@ var router = express.Router();
 var pool = require("../database/connect");
 var dataConnect = require("../database/index")
 /* GET users listing. */
-router.post('/getUser', function(req, res, next) {
-  // res.send('respond with a resource');
-  let name = "luffy"
+router.post('/getUserByPage', function(req, res, next) {
+  const {pageNo, pageSize} = req.body
+  let offset = pageNo - 1 < 0 ? 0 : pageNo -1
   dataConnect().then(conn => {
     // console.log(conn);
     return new Promise((resolve ,reject) => {
-      conn.query(`select * from user where name = "${name}"`, function (error, results, fields) {
+      conn.query(`select * from user limit ${offset},${pageSize}`, function (error, results, fields) {
         try {
           resolve(results);
           conn.release();
@@ -26,13 +26,23 @@ router.post('/getUser', function(req, res, next) {
 });
 router.post('/getUser1', function(req, res, next) {
   // res.send('respond with a resource');
-  let name = ["luffy"]
-  dataConnect(function (conn) {
-    conn.query('select * from user where name = ?', name, function (error, results, fields) {
-      res.send(results);
-      conn.release();
-      if (error) throw error;
+  let name = ['sanji']
+  dataConnect().then(conn => {
+    // console.log(conn);
+    return new Promise((resolve ,reject) => {
+      conn.query(`select * from user where name = ?`, name, function (error, results, fields) {
+        try {
+          resolve(results);
+          conn.release();
+          if (error) throw error;
+        } catch (error) {
+          reject(error);
+        }
+      })
     })
+  }).then(msg => {
+    console.log(msg)
+    res.send(msg);
   })
 });
 
