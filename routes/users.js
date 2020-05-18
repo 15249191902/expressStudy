0a534a2bc916ce0f9d4209ae3cf104f5942044a2
3172236129 <3172236129@qq.com>
2020-05-18 17:12:00 +0800

    ads

--- routes/users.js
+++ routes/users.js
@@ -12,9 +12,26 @@ async function getUserByPage (req, res, next) {
   } catch (error) {
     data = {msg: "消息失败"}
   }
-  next();
   return res.send(data);
+  next();
 }
+router.post("/deleteUserById", async function (req, res, next) {
+  let data = {};
+  try {
+    let {id} = req.body
+    data = await query(`delete from user where id = ?`,id);
+    if (data.affectedRows > 0) {
+      data = {msg: '删除成功！', code: 1}
+    } else {
+      data = {msg: '删除失败！', code: 0}
+    }
+  }catch (error) {
+    data = {msg: error, code: 0}
+  }
+  return res.send(data);
+  next();
+})
+// 封装前  旧版查询写法
 router.post('/getUser1', function(req, res, next) {
   // res.send('respond with a resource');
   let name = ['luffy']
---
+++ sql_statement/userAdd.sql
@@ -0,0 +1,6 @@
+-- 新增用户表单表
+insert user values 
+("1", "sanji", 16),
+("2", "wosupo", 16),
+("3", "zero", 16),
+("4", "bluke", 16)
